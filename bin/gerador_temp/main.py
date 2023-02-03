efa3bb22087e0786dff8e3fd232be3ba77da28f8
electric-rick <erick.augusto.ebs@gmail.com>
2023-02-03 14:43:04 -0300

    exercicios: adição de mais exercícios no diretório bin

---
+++ bin/gerador_temp/main.py
@@ -0,0 +1,6 @@
+# Descrição:
+#   1-Adicionar biblioteca do random
+#   2-Criar por classe o gerador de temperatura
+#   3-Criar lista com mínimos e máximos de temperatura do dia, tente utilizar métodos da classe. Dica: < [{min: temp_min, max: temp_max}] >
+#   4-Criar lista com todos os índices de temperatura com limite máximo 365.
+# Dica: Por convenção classes começam com letras maiusculas: < class Carro: > é um exemplo
