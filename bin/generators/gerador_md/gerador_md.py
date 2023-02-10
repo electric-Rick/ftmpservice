27a1b60884b7cb075edea9bf4ad9c3f8eb76896c
electric-rick <erick.augusto.ebs@gmail.com>
2023-02-10 14:40:47 -0300

    change: alteração dentro da pasta bin, mudança da nomenclatura dos generators. Nenhuma funcionalidade foi adicionada.

---
+++ bin/generators/gerador_md/gerador_md.py
@@ -0,0 +1,48 @@
+# INPUT MÊS E DIA:
+
+from gerador_temp import temperatura as Temp
+
+
+print(f.temps())
+
+class Date:
+
+    def __init__(self, dia, mes):
+        self.d = dia
+        self.m = mes
+        pass
+    
+    def traducao(mes):
+        lst = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','dezembro']
+        return lst[mes - 1]
+
+    def data():
+        while 1:
+            try:
+                mes = int(input('\nInsira o mês: '))
+                if mes < 1 or mes > 12:
+                    raise Exception
+                break
+            except:
+                print('\nMês inválido! Tente outro...')
+        while 2:
+            try:
+                dia = int(input('\nInsira o dia: '))
+                if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+                    if dia < 1 or dia > 31:
+                        raise Exception
+                    break
+                elif mes == 2:
+                    if dia < 1 or dia > 28:
+                        raise Exception
+                    break
+                else:
+                    if dia < 1 or dia > 30:
+                        raise Exception
+                    break
+            except:
+                print('\nDia inválido! Tente outro...')
+        return [mes,dia]
+
+
+
