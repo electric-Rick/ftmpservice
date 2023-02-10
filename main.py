59e991fb9854bcb79029aaa1dfcd7979d15bac99
electric-rick <erick.augusto.ebs@gmail.com>
2023-02-10 14:39:37 -0300

    fix: mudança no top level da aplicação, removendo linhas que não são mais utilizadas

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-#from bin.input_data import Data
-#from bin.Temperatura import Temperatura
 from bin.repl import main
-# Temperatura.planilha() = banco de dados
 
-print('\n')
-print('~'*70)
-
-print("Olá, usuário!\n Digite seu nome e senha para garantirmos os dados desta sessão: \n")
-
-user = input("username: ")
-pw   = input("pass: ")
-print("Nome e chave obtidas com sucesso, aguarde até o fim do processamento.")
-
-print('~'*70)
-print('\n')
